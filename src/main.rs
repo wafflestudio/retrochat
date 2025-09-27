a54eb9bb80681572c8851079d226e6ca503bee0a
hanan-nana <hanan.dev04@gmail.com>
2025-09-27 20:15:32 +0900

    Add GOOGLE_AI_API_KEY

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use clap::Parser;
 use retrochat::cli::Cli;
 
 fn main() -> anyhow::Result<()> {
+    // Load .env file if it exists (ignore errors if missing)
+    dotenvy::dotenv().ok();
+
     tracing_subscriber::fmt::init();
 
     let cli = Cli::parse();
